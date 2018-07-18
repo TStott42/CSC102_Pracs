0c6dd5c6136aa7e3561df1c8ede8732fee3aae88
Thomas Stott <popcuddz@gmail.com>
2018-07-18 15:52:34 +0200

    Commit

---
+++ Practical 1/Drip/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How often do you hear a drip drop <in seconds>? ");
+
+            String dripString = Console.ReadLine();
+            int dripInt = Convert.ToInt16(dripString);
+
+            Console.WriteLine("This wastes " + (3600 / dripInt * 0.00025) + " litres per hour");
+            Console.WriteLine("This wastes " + (86400 / dripInt * 0.00025) + " litres per day");
+            Console.WriteLine("This wastes " + (604800 / dripInt * 0.00025) + " litres per week");
+
+            Console.Read();
+
+
+        }
+    }
+}
